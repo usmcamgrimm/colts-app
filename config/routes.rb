4062815a52af8d0fc866ca5b1bc59349de60a5e5
Adrian Grimm <usmcamgrimm@gmail.com>
2023-07-04 13:57:05 -0400

    Add the header and configure routes to link between pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "players#index"
   get "players" => "players#index"
-  get "players/:id" => "players#show"
+  get "players/:id" => "players#show", as: "player"
 end
