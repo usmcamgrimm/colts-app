d7cbbf6b9c386d3737225a905280000dc48dda76
Adrian Grimm <usmcamgrimm@gmail.com>
2023-08-19 12:05:31 -0400

    Roster updates
    Added DJ Montgomery and James Washington
    Malik Turner waived
    Genard Avery to IR

--- db/seeds/003_injres.rb
+++ db/seeds/003_injres.rb
@@ -2,5 +2,6 @@ Player.create!([
   # Injured/Reserve
   { name: 'Ashton Dulin', number: '16', position: 'WR', exp: 5, status: 'IR', college: 'Malone University', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4061956.png&w=350&h=254' },
   { name: 'Daniel Scott', number: '31', position: 'S', exp: 0, status: 'IR', college: 'California', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4242415.png&w=350&h=254' },
-  { name: 'Jake Witt', number: '76', position: 'T', exp: 0, status: 'IR', college: 'Northern Michigan', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5140684.png&w=350&h=254' }
+  { name: 'Jake Witt', number: '76', position: 'T', exp: 0, status: 'IR', college: 'Northern Michigan', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5140684.png&w=350&h=254' },
+  { name: 'Genard Avery', number: '92', position: 'DE', exp: 6, status: 'Active', college: 'Memphis', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3126204.png&w=350&h=254' },
 ])
