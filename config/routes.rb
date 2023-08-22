df87fd67c9fe5094c5090f2a320801397b4f4016
Adrian Grimm <usmcamgrimm@gmail.com>
2023-08-22 09:27:03 -0400

    Update PUP for rookies and try fixing the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "players#index"
   get "players" => "players#index"
   get "players/:id" => "players#show", as: "player"
+  get "pups/:id" => "pups#show", as: "pup"
+  get "injureds/:id" => "injureds#show", as: "injured"
+  get "pracsquads/:id" => "pracsquads#show", as: "pracsquad"
 end
