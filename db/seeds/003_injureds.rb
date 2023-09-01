aa432193c97c25d999cadf8e697fc5c4008f75cb
Adrian Grimm <usmcamgrimm@gmail.com>
2023-09-01 06:31:59 -0400

    Update active roster and correct status on IR

--- db/seeds/003_injureds.rb
+++ db/seeds/003_injureds.rb
@@ -2,11 +2,11 @@ Injured.create!([
   # Injured/Reserve
   { name: 'Ashton Dulin', number: '16', position: 'WR', exp: 5, status: 'IR', college: 'Malone University', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4061956.png&w=350&h=254' },
   { name: 'Daniel Scott', number: '31', position: 'S', exp: 0, status: 'IR', college: 'California', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4242415.png&w=350&h=254' },
-  { name: 'Ricky Seals-Jones', number: '48', position: 'TE', exp: 6, status: 'Active', college: 'Texas A&M', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3051806.png&w=350&h=254' },
-  { name: 'JoJo Domann', number: '57', position: 'LB', exp: 2, status: 'Active', college: 'Nebraska', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4034841.png&w=350&h=254' },
-  { name: 'Danny Pinter', number: '63', position: 'G', exp: 4, status: 'Active', college: 'Ball State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3915470.png&w=350&h=254' },
+  { name: 'Ricky Seals-Jones', number: '48', position: 'TE', exp: 6, status: 'IR', college: 'Texas A&M', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3051806.png&w=350&h=254' },
+  { name: 'JoJo Domann', number: '57', position: 'LB', exp: 2, status: 'IR', college: 'Nebraska', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4034841.png&w=350&h=254' },
+  { name: 'Danny Pinter', number: '63', position: 'G', exp: 4, status: 'IR', college: 'Ball State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3915470.png&w=350&h=254' },
   { name: 'Jake Witt', number: '76', position: 'T', exp: 0, status: 'IR', college: 'Northern Michigan', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5140684.png&w=350&h=254' },
-  { name: 'Jelani Woods', number: '80', position: 'TE', exp: 2, status: 'Active', college: 'Virginia', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4241410.png&w=350&h=254' },
-  { name: 'Titus Leo', number: '91', position: 'DE', exp: 0, status: 'Active', college: 'Wagner', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4368468.png&w=350&h=254' },
+  { name: 'Jelani Woods', number: '80', position: 'TE', exp: 2, status: 'IR', college: 'Virginia', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4241410.png&w=350&h=254' },
+  { name: 'Titus Leo', number: '91', position: 'DE', exp: 0, status: 'IR', college: 'Wagner', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4368468.png&w=350&h=254' },
   { name: 'Genard Avery', number: '92', position: 'DE', exp: 6, status: 'IR', college: 'Memphis', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3126204.png&w=350&h=254' },
 ])
