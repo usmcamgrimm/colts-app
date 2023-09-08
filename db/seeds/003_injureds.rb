5eb75dd1629e326198e6fcc86bb301b32aae2722
Adrian Grimm <usmcamgrimm@gmail.com>
2023-09-08 06:08:52 -0400

    Update roster
    Remove R Seals-Jones

--- db/seeds/003_injureds.rb
+++ db/seeds/003_injureds.rb
@@ -2,7 +2,6 @@ Injured.create!([
   # Injured/Reserve
   { name: 'Ashton Dulin', number: '16', position: 'WR', exp: 5, status: 'IR', college: 'Malone University', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4061956.png&w=350&h=254' },
   { name: 'Daniel Scott', number: '31', position: 'S', exp: 0, status: 'IR', college: 'California', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4242415.png&w=350&h=254' },
-  { name: 'Ricky Seals-Jones', number: '48', position: 'TE', exp: 6, status: 'IR', college: 'Texas A&M', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3051806.png&w=350&h=254' },
   { name: 'JoJo Domann', number: '57', position: 'LB', exp: 2, status: 'IR', college: 'Nebraska', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4034841.png&w=350&h=254' },
   { name: 'Danny Pinter', number: '63', position: 'G', exp: 4, status: 'IR', college: 'Ball State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3915470.png&w=350&h=254' },
   { name: 'Jake Witt', number: '76', position: 'T', exp: 0, status: 'IR', college: 'Northern Michigan', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5140684.png&w=350&h=254' },
