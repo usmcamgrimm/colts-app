3cc116fe564fe9596a959be52327230537009b3e
Adrian Grimm <usmcamgrimm@gmail.com>
2023-09-10 05:11:58 -0400

    Elevated J Funk and J Winfree to the active roster.

--- db/seeds/004_pracsquads.rb
+++ db/seeds/004_pracsquads.rb
@@ -2,12 +2,10 @@ Pracsquad.create!([
   # Practice Squad
   { name: 'D.J. Montgomery', number: '2', position: 'WR', exp: 4, status: 'Practice Squad', college: 'Austin Peay', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4249030.png&w=350&h=254' },
   { name: 'Amari Rodgers', number: '3', position: 'WR', exp: 3, status: 'Practice Squad', college: 'Clemson', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4239992.png&w=350&h=254' },
-  { name: 'Juwann Winfree', number: '9', position: 'WR', exp: 3, status: 'Practice Squad', college: 'Colorado', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3128317.png&w=350&h=254' },
   { name: 'Racey MccMath', number: '15', position: 'WR', exp: 3, status: 'Practice Squad', college: 'LSU', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4242231.png&w=350&h=254' },
   { name: 'Mike Strachan', number: '17', position: 'WR', exp: 3, status: 'Practice Squad', college: 'Charleston', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4589245.png&w=350&h=254' },
   { name: 'Darren Hall', number: '30', position: 'CB', exp: 3, status: 'Practice Squad', college: 'San Diego State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4261606.png&w=350&h=254' },
-  { name: 'Jason Huntley', number: '31', position: 'RB', exp: 4, status: 'Practice Squad', college: 'New Mexico State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4040790.png&w=350&h=254' },
-  { name: 'Jake Funk', number: '34', position: 'RB', exp: 3, status: 'Practice Squad', college: 'Maryland', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4036224.png&w=350&h=254' },
+  { name: 'Jason Huntley', number: '31', position: 'RB', exp: 4, status: 'Practice Squad', college: 'New Mexico State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4040790.png&w=350&h=254' },,
   { name: 'Henry Black', number: '36', position: 'S', exp: 3, status: 'Practice Squad', college: 'Baylor', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3928920.png&w=350&h=254' },
   { name: 'Isaac Taylor-Stuart', number: '37', position: 'CB', exp: 2, status: 'Practice Squad', college: 'USC', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4374301.png&w=350&h=254' },
   { name: 'Marcel Dabo', number: '42', position: 'S', exp: 1, status: 'Practice Squad', college: 'Stuttgart', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5054377.png&w=350&h=254' },
