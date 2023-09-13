f5e5d1b5f36f0dd2ccea7f4ec4d1d1a8b34a9832
Adrian Grimm <usmcamgrimm@gmail.com>
2023-09-13 06:12:35 -0400

    Busy day for roster updates

--- db/seeds/003_injureds.rb
+++ db/seeds/003_injureds.rb
@@ -1,6 +1,7 @@
 Injured.create!([
   # Injured/Reserve
   { name: 'Ashton Dulin', number: '16', position: 'WR', exp: 5, status: 'IR', college: 'Malone University', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4061956.png&w=350&h=254' },
+  { name: 'Evan Hull', number: '26', position: 'RB', exp: 0, status: 'Active', college: 'Northwestern', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4569609.png&w=350&h=254' },
   { name: 'Daniel Scott', number: '31', position: 'S', exp: 0, status: 'IR', college: 'California', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4242415.png&w=350&h=254' },
   { name: 'Danny Pinter', number: '63', position: 'G', exp: 4, status: 'IR', college: 'Ball State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3915470.png&w=350&h=254' },
   { name: 'Jake Witt', number: '76', position: 'T', exp: 0, status: 'IR', college: 'Northern Michigan', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5140684.png&w=350&h=254' },
