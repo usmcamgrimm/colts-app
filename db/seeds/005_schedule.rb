f7da719e571865e0be8393ca029879af3568c09e
Adrian Grimm <usmcamgrimm@gmail.com>
2023-09-18 05:52:21 -0400

    Add game results vs Texans

--- db/seeds/005_schedule.rb
+++ db/seeds/005_schedule.rb
@@ -1,7 +1,7 @@
 Schedule.create!([
   # Regular Season Games
   { week: '1', gamedate: '10-09-2023', gametime: '1:00PM', location: 'home', result: 'L', score: '21-31', oppname: 'Jacksonville Jaguars', oppimage: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/jax.png&h=200&w=200' },
-  { week: '2', gamedate: '17-09-2023', gametime: '1:00PM', location: 'away', result: '', score: '', oppname: 'Houston Texans', oppimage: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/hou.png&h=200&w=200' },
+  { week: '2', gamedate: '17-09-2023', gametime: '1:00PM', location: 'away', result: 'W', score: '31-20 ', oppname: 'Houston Texans', oppimage: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/hou.png&h=200&w=200' },
   { week: '3', gamedate: '24-09-2023', gametime: '1:00PM', location: 'away', result: '', score: '', oppname: 'Baltimore Ravens', oppimage: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/bal.png&h=200&w=200' },
   { week: '4', gamedate: '01-10-2023', gametime: '1:00PM', location: 'home', result: '', score: '', oppname: 'LA Rams', oppimage: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/lar.png&h=200&w=200' },
   { week: '5', gamedate: '08-10-2023', gametime: '1:00PM', location: 'home', result: '', score: '', oppname: 'Tennessee Titans', oppimage: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/ten.png&h=200&w=200' },
