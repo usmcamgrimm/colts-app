a55e16b2b8c1f59acb849d55caee609f1b231b69
Adrian Grimm <usmcamgrimm@gmail.com>
2023-09-22 06:40:13 -0400

    Add an edit route and Edit Player button to the show template

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "players#index"
   get "players" => "players#index"
   get "players/:id" => "players#show", as: "player"
+  get "players/:id/edit" => "players#edit", as: "edit_player"
   get "pups/:id" => "pups#show", as: "pup"
   get "injureds/:id" => "injureds#show", as: "injured"
   get "pracsquads/:id" => "pracsquads#show", as: "pracsquad"
