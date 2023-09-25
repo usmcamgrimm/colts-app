8a1a928054a853d552f140921c8675514ef33c5b
Adrian Grimm <usmcamgrimm@gmail.com>
2023-09-25 05:57:55 -0400

    Revert "Pre-game roster updates"
    Back to the PS
    This reverts commit 1d3203e708da02903fba26dc7cd62521b8977e32.

--- db/seeds/004_pracsquads.rb
+++ db/seeds/004_pracsquads.rb
@@ -2,6 +2,7 @@ Pracsquad.create!([
   # Practice Squad
   { name: 'D.J. Montgomery', number: '2', position: 'WR', exp: 4, status: 'Practice Squad', college: 'Austin Peay', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4249030.png&w=350&h=254' },
   { name: 'Amari Rodgers', number: '3', position: 'WR', exp: 3, status: 'Practice Squad', college: 'Clemson', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4239992.png&w=350&h=254' },
+  { name: 'Juwann Winfree', number: '9', position: 'WR', exp: 3, status: 'Practice Squad', college: 'Colorado', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3128317.png&w=350&h=254' },
   { name: 'Racey MccMath', number: '15', position: 'WR', exp: 3, status: 'Practice Squad', college: 'LSU', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4242231.png&w=350&h=254' },
   { name: 'Darren Hall', number: '30', position: 'CB', exp: 3, status: 'Practice Squad', college: 'San Diego State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4261606.png&w=350&h=254' },
   { name: 'Tyler Goodson', number: '31', position: 'RB', exp: 1, status: 'Practice Squad', college: 'Iowa', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4429676.png&w=350&h=254' },
@@ -13,6 +14,7 @@ Pracsquad.create!([
   { name: 'Dakoda Shepley', number: '65', position: 'C', exp: 2, status: 'Active', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4335942.png&w=350&h=254' },
   { name: 'Lewis Kidd', number: '66', position: 'G', exp: 2, status: 'Practice Squad', college: 'Montana State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4047523.png&w=350&h=254' },
   { name: 'Jack Anderson', number: '67', position: 'C', exp: 3, status: 'Practice Squad - Injured', college: 'Texas Tech', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4241844.png&w=350&h=254' },
+  { name: 'Ike Boettger', number: '68', position: 'G', exp: 6, status: 'Practice Squad', college: 'Iowa', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3040166.png&w=350&h=254' },
   { name: 'Jordan Murray', number: '84', position: 'TE', exp: 0, status: 'Practice Squad', college: 'Hawaii', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/college-football/players/full/4368172.png&w=350&h=254' },
   { name: 'Al-Quadin Muhammad', number: '97', position: 'DE', exp: 7, status: 'Practice Squad', college: 'Miami', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3051942.png&w=350&h=254' },
   { name: 'McTelvin Agim', number: '98', position: 'DT', exp: 3, status: 'Practice Squad', college: 'Arkansas', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4035566.png&w=350&h=254' }
