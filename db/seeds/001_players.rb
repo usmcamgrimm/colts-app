bae0ab6cec0f76482bdfab162f37ef8668704948
Adrian Grimm <usmcamgrimm@gmail.com>
2023-10-03 06:06:45 -0400

    Roster update
    Add KJ Hamler to PS
    Removed D Hall from PS
    D Flowers out for season

--- db/seeds/001_players.rb
+++ db/seeds/001_players.rb
@@ -16,7 +16,6 @@ Player.create!([
   { name: 'Trey Sermon', number: '27', position: 'RB', exp: 3, status: 'Active', college: 'Ohio State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4241401.png&w=350&h=254' },
   { name: 'JuJu Brents', number: '29', position: 'CB', exp: 0, status: 'Active', college: 'Kansas State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4360488.png&w=350&h=254' },
   { name: 'Julian Blackmon', number: '32', position: 'S', exp: 4, status: 'Active', college: 'Utah', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4035661.png&w=350&h=254' },
-  { name: 'Dallis Flowers', number: '33', position: 'CB', exp: 2, status: 'Active', college: 'Pittsburg State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4917592.png&w=350&h=254' },
   { name: 'Jake Funk', number: '34', position: 'RB', exp: 3, status: 'Practice Squad', college: 'Maryland', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4036224.png&w=350&h=254' },
   { name: 'Tony Brown', number: '38', position: 'CB', exp: 5, status: 'Active', college: 'Alabama', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3115308.png&w=350&h=254' },
   { name: 'Darrell Baker Jr.', number: '39', position: 'CB', exp: 1, status: 'Active', college: 'Georgia Southern', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4036660.png&w=350&h=254' },
--- db/seeds/003_injureds.rb
+++ db/seeds/003_injureds.rb
@@ -3,6 +3,7 @@ Injured.create!([
   { name: 'Ashton Dulin', number: '16', position: 'WR', exp: 5, status: 'IR', college: 'Malone University', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4061956.png&w=350&h=254' },
   { name: 'Evan Hull', number: '26', position: 'RB', exp: 0, status: 'Active', college: 'Northwestern', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4569609.png&w=350&h=254' },
   { name: 'Daniel Scott', number: '31', position: 'S', exp: 0, status: 'IR', college: 'California', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4242415.png&w=350&h=254' },
+  { name: 'Dallis Flowers', number: '33', position: 'CB', exp: 2, status: 'IR', college: 'Pittsburg State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4917592.png&w=350&h=254' },
   { name: 'Danny Pinter', number: '63', position: 'G', exp: 4, status: 'IR', college: 'Ball State', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3915470.png&w=350&h=254' },
   { name: 'Jake Witt', number: '76', position: 'T', exp: 0, status: 'IR', college: 'Northern Michigan', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/5140684.png&w=350&h=254' },
   { name: 'Jelani Woods', number: '80', position: 'TE', exp: 2, status: 'IR', college: 'Virginia', photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4241410.png&w=350&h=254' },
