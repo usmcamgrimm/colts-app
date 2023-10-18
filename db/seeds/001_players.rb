e1dd8019a13623c2c52538f53b9686c18647240b
Adrian Grimm <usmcamgrimm@gmail.com>
2023-10-18 09:56:34 -0400

    Roster Update
    Signed J Winfree to active roster
    G Stewart suspended. Uncommented the suspended section of the index template

--- db/seeds/001_players.rb
+++ db/seeds/001_players.rb
@@ -86,6 +86,15 @@ Player.create!([
     punt_lng: 54
   },
   { 
+    name: 'Juwann Winfree',
+    number: '9',
+    position: 'WR',
+    exp: 3,
+    status: 'Active',
+    college: 'Colorado',
+    photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3128317.png&w=350&h=254'
+  },
+  { 
     name: 'Gardner Minshew', 
     number: '10', 
     position: 'QB', 
@@ -591,21 +600,6 @@ Player.create!([
     rec: 2, 
     rec_yards: 49, 
     rec_td: 0 
-  },
-  { 
-    name: 'Grover Stewart', 
-    number: '90', 
-    position: 'DT', 
-    exp: 7, 
-    status: 'Active', 
-    college: 'Albany State', 
-    photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4058825.png&w=350&h=254',
-    tackles: 20,
-    solo_tackles: 13,
-    assist: 7,
-    def_sacks: 0.0,
-    safety: 0,
-    def_fumble: 0 
   },
   { 
     name: 'Jake Martin', 
