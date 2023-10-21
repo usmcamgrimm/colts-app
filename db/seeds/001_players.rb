b41e22f546ee094115e50d28c885e504ba7a7a2f
Adrian Grimm <usmcamgrimm@gmail.com>
2023-10-21 09:52:19 -0400

    Corrected JT status

--- db/seeds/001_players.rb
+++ db/seeds/001_players.rb
@@ -212,7 +212,7 @@ Player.create!([
     number: '28', 
     position: 'RB', 
     exp: 4, 
-    status: 'PUP', 
+    status: 'Active', 
     college: 'Wisconsin', 
     photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4242335.png&w=350&h=254', 
     rec: 6, 
