8d7087963f18dd01087cacd458d4b3607fc43f1f
Adrian Grimm <usmcamgrimm@gmail.com>
2023-11-18 10:22:05 -0500

    Fixing the build command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle exec rails db:environment:set RAILS_ENV=production
+bundle exec rake db:schema:load
+# bundle exec rake db:migrate
