4c989488337226f98f2fc9b9ffa2baaa569ca1fe
Adrian Grimm <usmcamgrimm@gmail.com>
2023-11-22 10:32:29 -0500

    Let's do this again

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # bundle exec rails db:environment:set RAILS_ENV=production
-# bundle exec rake db:schema:load
-bundle exec rake db:migrate
+bundle exec rake db:schema:load DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# bundle exec rake db:migrate
