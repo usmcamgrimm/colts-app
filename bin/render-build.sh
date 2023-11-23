4548fa8ce99b952687abff746330750ad3fbbcd3
Adrian Grimm <usmcamgrimm@gmail.com>
2023-11-23 09:00:29 -0500

    Try this

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # bundle exec rails db:environment:set RAILS_ENV=production
+bundle exec rake db:create
 bundle exec rake db:schema:load DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rake db:seed
 # bundle exec rake db:migrate
