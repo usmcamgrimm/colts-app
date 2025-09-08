3127f5e8e78c804ae879d62f5cde96716b3b8c2d
Adrian Grimm <usmcamgrimm@gmail.com>
2025-09-08 10:16:26 -0400

    Add migrations for kick return yards and punt return yards

---
+++ db/migrate/20250908141043_add_kr_yds_to.rb
@@ -0,0 +1,4 @@
+class AddKrYdsTo < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_08_115447) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_08_141108) do
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "number"
