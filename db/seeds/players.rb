9ded53ebc2542b65ac55677cbba8e20fe9d3943c
Adrian Grimm <usmcamgrimm@gmail.com>
2026-09-13 10:29:12 -0400

    Game day activation

--- db/seeds/players.rb
+++ db/seeds/players.rb
@@ -212,7 +212,7 @@ Player.create!([
     number: '29',
     position: 'CB',
     exp: 0,
-    status: 'PS',
+    status: 'Active',
     college: 'Oklahoma State',
     photo: 'https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/4685970.png&w=350&h=254'
   },
